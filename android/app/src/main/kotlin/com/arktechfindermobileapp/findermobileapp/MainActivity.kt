2232e634bc99823ecb3fa9110ed89b4c396d7e76
bkantwi <bernardantwi2010@gmail.com>
2022-11-19 15:17:18 +0000

    initial

---
+++ android/app/src/main/kotlin/com/arktechfindermobileapp/findermobileapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arktechfindermobileapp.findermobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'src/web_view_stack.dart';  // Add this import
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: const WebViewStack(),   // Replace the WebView widget with WebViewStack
+    );
+  }
+}
