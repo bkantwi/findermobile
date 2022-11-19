3189918fd3c0af517cc4cd984c83061ab7ae7e97
bkantwi <bernardantwi2010@gmail.com>
2022-11-19 15:38:19 +0000

    controls added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'src/navigation_controls.dart';
 import 'src/web_view_stack.dart';
 
 void main() {
@@ -17,13 +20,20 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();    // Instantiate the controller
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
+        // Add from here ...
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+        // ... to here.
       ),
-      body: const WebViewStack(),
+      body: WebViewStack(controller: controller),       // Add the controller argument
     );
   }
 }
