8a96505001976d032252c51d6c3696c19888c9d1
bkantwi <bernardantwi2010@gmail.com>
2022-11-19 15:50:02 +0000

    convert menu to stateful widget

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -4,17 +4,23 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 enum _MenuOptions {
   navigationDelegate,
+  userAgent
 }
 
-class Menu extends StatelessWidget {
+class Menu extends StatefulWidget {                   // Convert to StatefulWidget
   const Menu({required this.controller, super.key});
 
   final Completer<WebViewController> controller;
 
   @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<WebViewController>(
-      future: controller.future,
+      future: widget.controller.future,
       builder: (context, controller) {
         return PopupMenuButton<_MenuOptions>(
           onSelected: (value) async {
@@ -22,12 +28,24 @@ class Menu extends StatelessWidget {
               case _MenuOptions.navigationDelegate:
                 await controller.data!.loadUrl('https://youtube.com');
                 break;
+              case _MenuOptions.userAgent:
+                final userAgent = await controller.data!
+                    .runJavascriptReturningResult('navigator.userAgent');
+                if (!mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(userAgent),
+                ));
+                break;
             }
           },
           itemBuilder: (context) => [
             const PopupMenuItem<_MenuOptions>(
               value: _MenuOptions.navigationDelegate,
               child: Text('Navigate to YouTube'),
+            ),
+            const PopupMenuItem<_MenuOptions>(
+              value: _MenuOptions.userAgent,
+              child: Text('Show user-agent'),
             ),
           ],
         );
